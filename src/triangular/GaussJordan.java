3b91912aca565b4ebbef0453a1a247f27e7de237
mauroat <mauroatrotta@gmail.com>
2016-09-07 19:20:01 -0300

    Ultima clase 06-09-16
    Se agregan las modificaciones hechas en clase el 06-09-16

--- src/triangular/GaussJordan.java
+++ src/triangular/GaussJordan.java
@@ -5,7 +5,7 @@ import selMath.MatrizMath;
 
 public class GaussJordan implements Triangulador{
 	@Override
-	public MatrizMath triangular(MatrizMath m) throws Exception{
+	public MatrizMath (MatrizMath m) throws Exception{
 		int[] size = m.
 	}
 	
